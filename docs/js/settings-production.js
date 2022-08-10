611a09e0e85e060ea04c39ca6138f3306435d0f6
Surviville <ultimatesurviville@gmail.com>
2022-08-10 17:52:52 +0200

    Small fixes

--- docs/js/settings-production.js
+++ docs/js/settings-production.js
@@ -1,11 +1,12 @@
 // Settings for testnet (SmartBCH Amber, NetID 10001)
 // deployed on testnet from 0xE0d61627d14F1E6C1C5FABDD219dF0a003C9a0cC
 
-window.CardsAddress        = '0xE7B1076d0853a7aAa6FbB477886726ed03C6Fffd'; // wrong address -- not deployed yet
-window.RewardTokenAddress  = '0xE7B1076d0853a7aAa6FbB477886726ed03C6Fffd';
-window.ReaperAddress       = '0xb1C2103F29341C0E62aE88a5d13D84b5ea6757A9';
-window.DevAddress          = '0xd54265a68240c236d46E525B85E2F247c5d0dB23';
-window.rpcServer           = 'https://smartbch.fountainhead.cash/mainnet';
-window.netId               = 10000;
-window.netName             = "SmartBch";
-window.minHarvest          = 1;
+window.ca = {};
+window.ca['cards'] = '0xE7B1076d0853a7aAa6FbB477886726ed03C6Fffd'; // wrong address -- not deployed yet
+window.ca['svc'] = '0xE7B1076d0853a7aAa6FbB477886726ed03C6Fffd';
+window.ca['reaper'] = '0xb1C2103F29341C0E62aE88a5d13D84b5ea6757A9';
+window.ca['dev'] = '0xd54265a68240c236d46E525B85E2F247c5d0dB23';
+window.rpcServer = 'https://smartbch.fountainhead.cash/mainnet';
+window.netId = 10000;
+window.netName = "SmartBch";
+window.minHarvest = 1;
--- docs/js/settings-test.js
+++ docs/js/settings-test.js
@@ -1,15 +1,17 @@
 // Settings for testnet (SmartBCH Amber, NetID 10001)
 // deployed on testnet from 0xE0d61627d14F1E6C1C5FABDD219dF0a003C9a0cC
 
-window.RT30StakingAddress = '';
-window.RT14StakingAddress = '';
-window.RT7StakingAddress = '';
-window.RT0StakingAddress = '0x60379Be2c107Bd9E23BaE62982db9BeBC73320aF';
-window.CardsAddress = '0x2e825888bB89fFcaA68f0b6A7EB19D334C5f885B';
 
-window.RewardTokenAddress = '0x04B51E3DAfa23Bb7a5395fc631b69258191B7B88'; // SVC
-window.ReaperAddress = '0xC6b49F643e9E67157D51076736AC7351d30B3057';
-window.DevAddress = '0xE0d61627d14F1E6C1C5FABDD219dF0a003C9a0cC';
+window.ca = {};
+window.ca['svc30Staking'] = '';
+window.ca['svc4Staking'] = '';
+window.ca['svc7Staking'] = '';
+window.ca['svc0Staking'] = '0x60379Be2c107Bd9E23BaE62982db9BeBC73320aF';
+window.ca['cards'] = '0x2e825888bB89fFcaA68f0b6A7EB19D334C5f885B';
+
+window.ca['svc'] = '0x04B51E3DAfa23Bb7a5395fc631b69258191B7B88';
+window.ca['reaper'] = '0xC6b49F643e9E67157D51076736AC7351d30B3057';
+window.ca['dev'] = '0xE0d61627d14F1E6C1C5FABDD219dF0a003C9a0cC';
 window.rpcServer = 'https://moeing.tech:9545';
 window.netId = 10001;
 window.netName = "SmartBch Testnet";
