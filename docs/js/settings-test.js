279b893608e5cda239a4d690f64e001c6e3b01f5
Surviville <ultimatesurviville@gmail.com>
2022-08-15 18:13:02 +0200

    New cards contract

--- docs/js/settings-test.js
+++ docs/js/settings-test.js
@@ -7,7 +7,7 @@ window.ca['svc30Staking'] = '';
 window.ca['svc4Staking'] = '';
 window.ca['svc7Staking'] = '';
 window.ca['svc0Staking'] = '0x60379Be2c107Bd9E23BaE62982db9BeBC73320aF';
-window.ca['cards'] = '0x2e825888bB89fFcaA68f0b6A7EB19D334C5f885B';
+window.ca['cards'] = '0xbD15B3fc2f4B2B1364D31795BB55e74e786a8DCd';
 
 window.ca['svc'] = '0x04B51E3DAfa23Bb7a5395fc631b69258191B7B88';
 window.ca['reaper'] = '0xC6b49F643e9E67157D51076736AC7351d30B3057';
